5d177728d49ef15cb7ca6e7998121a6732f826f0
jishuya <jishuya3015@gmail.com>
2025-07-15 15:24:10 +0900

    Add isNew option

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -5,7 +5,11 @@ import './App.css'
 export default function AppProfile() {
     return (
         <div>
-            <Profile />
+            <Profile 
+            image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80' 
+            name='James' title='프론트앤드 개발자' isNew={true}/>
+            <Profile image='https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' name='Tom' title='백앤드 개발자'/>
+            <Profile image='https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' name='Susan' title='엔지니어'/>
         </div>
     );
 }
