6e97bc453edfd478c287407877ee00a55438fe9f
jishuya <jishuya3015@gmail.com>
2025-07-16 23:03:05 +0900

    Upgrade useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppCounter from './AppCounter'
-import AppUseEffect from './AppUseEffect'
-// import App from './App.jsx'
+import AppXY from './AppXY'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppCounter /> */}
-    <AppUseEffect />
+    <AppXY />
   </StrictMode>,
 )
