1237f7f52bbae98c12c325d7b4b3298c6206fe67
jishuya <jishuya3015@gmail.com>
2025-07-20 14:23:31 +0900

    How to make From

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppMentorsImmer from './AppMentorsImmer';
+import AppForm from './AppForm';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppCounter /> */}
-    <AppMentorsImmer />
+    <AppForm />
   </StrictMode>,
 )
