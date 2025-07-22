7e79d1b060a4b52d8560c4c8d86b37d1ee41096e
jishuya <jishuya3015@gmail.com>
2025-07-22 14:17:34 +0900

    Higher-Order Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppForm from './AppForm';
+import AppWrap from './AppWrap';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppCounter /> */}
-    <AppForm />
+    <AppWrap />
   </StrictMode>,
 )
