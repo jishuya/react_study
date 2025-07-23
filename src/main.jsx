31469772d2db1759d51a7f8642144bc93e24fca9
jishuya <jishuya3015@gmail.com>
2025-07-23 15:25:20 +0900

    Add Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppWrap from './AppWrap';
+import AppTheme from './AppTheme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppCounter /> */}
-    <AppWrap />
+    <AppTheme />
   </StrictMode>,
 )
