7cadf4dbe1fa0f5e1254e516266954085eb22b8a
jishuya <jishuya3015@gmail.com>
2025-07-28 14:30:22 +0900

    Make Custom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppTheme from './AppTheme';
+import AppProduct from './AppProduct';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <AppCounter /> */}
-    <AppTheme />
+    <AppProduct />
   </StrictMode>,
 )
